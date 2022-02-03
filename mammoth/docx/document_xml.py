40855d07435adae7a101020f88f93b76c58ff6f4
Garth Smedley <garth.smedley@gmail.com>
2022-02-02 21:29:52 -0400

    Fix crash converting docx file with odd element with no body

--- mammoth/docx/document_xml.py
+++ mammoth/docx/document_xml.py
@@ -6,14 +6,19 @@ def read_document_xml_element(
         body_reader,
         notes=None,
         comments=None):
-    
+
     if notes is None:
         notes = []
     if comments is None:
         comments = []
-    
+
     body_element = element.find_child("w:body")
-    return body_reader.read_all(body_element.children) \
+    if body_element is None:
+        children = []
+    else:
+        children = body_element.children
+
+    return body_reader.read_all(children) \
         .map(lambda children: documents.document(
             children,
             notes=documents.notes(notes),
